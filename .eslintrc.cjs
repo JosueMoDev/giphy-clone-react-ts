7f168e5ded5d7376fdce0445ae3d2ddc76c254db
Jonas Morales <josuemodev@gmail.com>
2023-05-20 09:52:14 -0600

    first commit eslint prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,35 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  overrides: [
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+    ecmaFeatures: {
+      tsx: true,
+      jsx: true
+    }
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  plugins: [
+    'react'
+  ],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'react/prop-types':'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import/no-absolute-path':'off'
   },
 }
